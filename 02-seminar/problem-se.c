43b18d931fb8adb77f4bb3f596fa047ba23c78a8
Perevozchikov Andrei <perevozchikov.andrey@gmail.com>
2023-11-14 21:40:41 +0100

    Sieve Eratosphena, all simple numbers

---
+++ 02-seminar/problem-se.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <assert.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+typedef struct Array
+{
+    char* arr;
+    unsigned length;
+} Array;
+
+void printSieve(Array* sieveArr)
+{
+    for (unsigned i = 2; i < sieveArr->length; ++i)
+    {
+        if (sieveArr->arr[i] == '\0')
+        {
+            printf("%d, ", i);
+        }
+    }
+}
+
+void fillSieve(Array* sieveArr)
+{
+    for (unsigned i = 2; i < sieveArr->length; ++i)
+    {
+        if (sieveArr->arr[i] != '1')
+        {
+            /* sieveArr->arr[i] = '1'; */
+            for (unsigned k = i * i; k < sieveArr->length; k = k + i)
+            {
+                sieveArr->arr[k] = '1';
+            }
+        }
+    }
+}
+
+int main()
+{
+    unsigned n;
+    scanf("%d", &n);
+    char* arr = (char*) calloc(n, sizeof(char));
+    if (!arr) return 1;
+
+    Array sieveArr = {arr, n};
+    /* for (int i = 0; i < sieveArr.length; i++) */
+    /* { */
+    /*     printf("%c, ", arr[i]); */
+    /* } */
+    fillSieve(&sieveArr);
+    printSieve(&sieveArr);
+    free(arr);
+    printf("\n");
+    return 0;
+}
